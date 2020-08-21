77517177bbdbf9bb240cb9825924ff35775ee09a
Bene Arinci <benedetta.arinci@gmail.com>
2020-08-21 15:15:56 +0100

    added robots.js file and used for adding props to card. Right now props are hardcoded

--- src/Card.js
+++ src/Card.js
@@ -2,12 +2,12 @@
 import React from 'react';
 //we need to import react so our app is able to understand JSX
 
-const Card = () => {
+const Card = (props) => {
   return (
       <div className='bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5'>
-        <img src='https://robohash.org/test?200x200' alt='robots'/>
-        <h2>Jane Doe</h2>
-        <p>jane.doe@gmail.com</p>
+        <img src={`https://robohash.org/${props.id}?200x200`} alt='robots'/>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
       </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './Card.js'
+import Card from './Card'
+import { robots } from './robots'
 import 'tachyons'
 import './index.css';
 import * as serviceWorker from './serviceWorker';
@@ -8,9 +9,9 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <div>
-    <Card />
-    <Card />
-    <Card />
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
     </div>
   </React.StrictMode>,
   document.getElementById('root')
