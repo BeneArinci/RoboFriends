607f3efd7691295585f6cc1f8f5e8040522dc668
Bene Arinci <benedetta.arinci@gmail.com>
2020-08-21 15:03:11 +0100

    Added multiple cards to the page and changed style

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Card = () => {
   return (
-      <div>
+      <div className='bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5'>
         <img src='https://robohash.org/test?200x200' alt='robots'/>
         <h2>Jane Doe</h2>
         <p>jane.doe@gmail.com</p>
