47b82508d3ee4b1c6b27d86faed796d50e077488
Bene Arinci <benedetta.arinci@gmail.com>
2020-08-21 17:37:03 +0100

    CardList code refactoring

--- src/CardList.js
+++ src/CardList.js
@@ -3,17 +3,19 @@ import Card from './Card.js';
 
 
 const CardList = ({robots}) => {
-  const cardsArray = robots.map((user, i) => {
-    return <Card 
-      key={i} 
-      id={robots[i].id} 
-      name={robots[i].name} 
-      email={robots[i].email}
-    />
-  })
   return (
     <div>
-      {cardsArray}
+      { 
+        robots.map((user, i) => {
+          return (
+          <Card 
+            key={i} 
+            id={robots[i].id} 
+            name={robots[i].name} 
+            email={robots[i].email}
+          />)
+        })
+      }
     </div>
   )
 }
