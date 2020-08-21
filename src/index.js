1b33c27256094fb5285f912a964dd921b84e214a
Bene Arinci <benedetta.arinci@gmail.com>
2020-08-21 12:33:01 +0100

    Used props for the first time

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello />
+    <Hello greeting={'Hello React Ninja'}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
