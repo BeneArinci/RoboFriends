2c2e7e3cacae694bce494f3478b9ef13e9f91b5c
Bene Arinci <benedetta.arinci@gmail.com>
2020-08-21 16:33:18 +0100

    Destructured props

--- src/Card.js
+++ src/Card.js
@@ -2,12 +2,12 @@
 import React from 'react';
 //we need to import react so our app is able to understand JSX
 
-const Card = (props) => {
+const Card = ({name, email, id}) => {
   return (
       <div className='bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5'>
-        <img src={`https://robohash.org/${props.id}?200x200`} alt='robots'/>
-        <h2>{props.name}</h2>
-        <p>{props.email}</p>
+        <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     )
 }
