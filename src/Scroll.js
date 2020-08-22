aebdfc12de138c935e4367fe4a82d8412df24c5e
Bene Arinci <benedetta.arinci@gmail.com>
2020-08-22 17:29:46 +0100

    ultimated the scroll component + styling

--- src/Scroll.js
+++ src/Scroll.js
@@ -3,7 +3,11 @@ import { render } from 'react-dom';
 
 const Scroll = (props) => {
   //props children refers to anything is wrapped into a non self closing component. In our case is the cardlist
-  render(props.children)
-}
+  return(
+    <div className='tc' style = {{overflow: 'scroll', border: '1px solid blue', height: '1000px', width: '70%', margin:'auto'}}>
+      {props.children}
+    </div>
+  );
+  }
 
 export default Scroll
